d3762c20a4098cc9247e1a19559a7e89a44fe193
unknown <fouad.hannoun@outlook.com>
2022-04-18 10:47:16 +0200

    adding calculate rewards and claim rewards functions

--- contracts/RingStaking.sol
+++ contracts/RingStaking.sol
@@ -30,9 +30,11 @@ contract RingFarm is Ownable {
 
     uint256 totalStaked; // total amount of staked tokens
     uint256 rewardsBalance; // total amount of reward tokens
+    uint256 public totalClaimed; // total amount of claimed rewards
     uint256 totalPendingRewards; // total amount of rewards to be distributed
 
     uint256 private rewardsPerDay; // rewards per day
+    uint256 public lastTimeStamp; // last timestamp on which rewards were updated
     address[] public stakers; // array of stakers addresses used to iterate over map
     // owner -> balance
     mapping(address => stakingInfo) stakingDetails;  // map containing the details of each user (check the struct)
@@ -97,8 +99,27 @@ contract RingFarm is Ownable {
     // calculate rewards
     //TO DO: Call this function at every block
     function calculateRewards() private {
-
+        address stakerAddress;
+        uint256 stakerShare;
+        for (uint i=0; i<stakers.length; i++) {
+            stakerAddress = stakers[i];
+            stakerShare = stakingDetails[stakerAddress].stakingBalance / totalStaked;
+            //staking rewards of each user is increased by the staker's share x rewards per day x seconds passed since last update divided by seconds per day.
+            stakingDetails[stakerAddress].stakingRewards += stakerShare * rewardsPerDay * (block.timestamp - lastTimeStamp)/86400;
+        }
     }
+
+    // claim rewards
+    function claimRewards(uint256 amount) public {
+        require(stakingDetails[msg.sender].stakerMarked); // require that the caller is a staker
+        require(amount <= stakingDetails[msg.sender].stakingRewards);
+        IERC20(rewardToken).transferFrom(address(this), msg.sender, amount);
+        rewardsBalance -= amount;
+        totalPendingRewards -= amount;
+        totalClaimed += amount;
+    }
+
+    
 
     // changeAllowedToken
     function changeAllowedToken(address token) public onlyOwner {
